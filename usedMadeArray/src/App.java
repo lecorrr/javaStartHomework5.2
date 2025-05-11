7fc3122097da4c07f1edd6e408bcb9e6034acf2c
lecor <88902282+lecorrr@users.noreply.github.com>
2025-05-11 07:40:30 +0000

    Update App.java

--- usedMadeArray/src/App.java
+++ usedMadeArray/src/App.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         ArrayList <Integer> arr = new ArrayList<>();
